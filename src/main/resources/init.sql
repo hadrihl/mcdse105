877e1bbeec018a7a48aceeb502574d84c93b5a01
hadrihl <hadrihilmi@gmail.com>
2024-02-18 13:05:22 +0800

    Update sql query for sample dataset.

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,3 +1,5 @@
+create database `mcdse105`;
+use `mcdse105`;
 create table `product` (
 	id bigint not null auto_increment,
     name varchar(200) not null,
@@ -7,3 +9,7 @@ create table `product` (
     quantity int,
     primary key (id)
 );
+insert into `product` (name, description, category, price, quantity)
+values ("Dendeng Nyet Berapi", "Dendeng is a classic dark and oily dish made up of tender beef marinated in spices and herbs and is usually served with rice and any other foods.", "Canned and Packed Food", 15, 1000);
+insert into `product` (name, description, category, price, quantity)
+values ("sambal Nyet Berapi", "Spicy and fragrant sambal original made by KA which best served with rice", "Canned and Packed Food", 16, 1000);
