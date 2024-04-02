2cba85b881af853f378aed83cccb3f4527d77187
hadrihl <hadrihl@outlook.com>
2024-04-02 14:55:20 +0800

    Fixed init sql query.

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -14,6 +14,14 @@ values ("Dendeng Nyet Berapi", "Dendeng is a classic dark and oily dish made up 
 insert into `product` (name, description, category, price, quantity)
 values ("sambal Nyet Berapi", "Spicy and fragrant sambal original made by KA which best served with rice", "Canned and Packed Food", 16, 1000);
 
+create table `user` (
+	id bigint not null auto_increment, 
+	username varchar(200) not null,
+	email varchar(200) not null,
+	password varchar(64) not null,
+	primary key (id)
+);
+
 create table `role` (
 	id int not null auto_increment,
     name varchar(20) not null,
